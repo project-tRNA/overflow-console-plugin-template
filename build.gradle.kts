eed9d96a8b272b1ce51caa0a136f459013ee804c
MrXiaoM <coolxiaom95@gmail.com>
2023-12-02 21:17:54 +0800

    adapt overflow core

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-    val kotlinVersion = "1.6.10"
+    val kotlinVersion = "1.8.0"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
 
-    id("net.mamoe.mirai-console") version "2.13.2"
+    id("net.mamoe.mirai-console") version "2.16.0"
 }
 
 group = "org.example"
@@ -14,4 +14,21 @@ repositories {
         maven("https://maven.aliyun.com/repository/public") // 阿里云国内代理仓库
     }
     mavenCentral()
+}
+
+mirai {
+    noTestCore = true
+    setupConsoleTestRuntime {
+        // 移除 mirai-core 依赖
+        classpath = classpath.filter {
+            !it.nameWithoutExtension.startsWith("mirai-core-jvm")
+        }
+    }
+}
+
+dependencies {
+    
+    val overflowVersion = "2.16.0"
+    compileOnly("top.mrxiaom:overflow-core-api:$overflowVersion")
+    testConsoleRuntime("top.mrxiaom:overflow-core:$overflowVersion")
 }
